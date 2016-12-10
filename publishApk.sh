5e2f305518caf0b52fbcfc6ae136cf49197669e5
lichenghang <lichenghang@wanda.cn>
2016-12-10 12:07:45 +0800

    apk sh.

--- publishApk.sh
+++ publishApk.sh
@@ -12,41 +12,80 @@ echo ${uploadApk}
 
 
 function uploadToPgyer {
+
+checkCmdIfExists jq
+
 uploadApk=$1
-uKey=""
-apiKey=""
+
+apiKey="55c2a4cbc4726d35f7740ff61a60061b"
+uKey="230f24fc2963f346d21e9364dea95be2"
+
 url="http://www.pgyer.com/apiv1/app/upload"
 
 response=$(curl -F "file=@${uploadApk}" -F "uKey=${uKey}" -F "_api_key=${apiKey}" ${url})
-#echo ${response}
+echo ${response}
 code=`echo ${response} | jq '.code'`
 message=`echo ${response} | jq '.message'`
 
 if [ $code -eq 0 ]
 then
 echo "-------------------------upload success----------------------"
+sendMyEmail response
+
 else
-echo "upload fail:$message"
+echo "--------------------upload fail:$message-----------------------"
+exit
 fi
 
 
 }
 
-function checkJq {
 
-res=`type jq`
+function checkCmdIfExists {
+
+cmd=$1
+res=`type ${cmd}`
 
 if [ $? -ne 0 ]
 then
 
-echo "jq not exists,please install jq first !!!!!!"
+echo "${cmd} not exists,please install ${cmd} first !!!!!!"
 exit
 
 fi
 
 }
 
+function sendMyEmail {
+
+checkCmdIfExists sendemail
+
+response=$1
+
+content="appName gerrit\n
+appVersion git.intra.ffan.com\n
+appVersionNo ${gerritUsername}"
+
+curl -o qr.jpg https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo_top_ca79a146.png
+
+sendemail -s mail.sohu.com \
+-f lchli888@sohu.com \
+-t lchli888@sohu.com \
+-u "apk build successful." \
+-m $content \
+-o message-charset="utf-8" \
+-xu lchli888@sohu.com \
+-xp lchli878266 \
+-o tls=no \
+-a qr.jpg
+
+
+}
+
+
+
 source ~/.bashrc
+
 npm install
 
 isUpload=${isUpload}
@@ -61,7 +100,7 @@ gradle clean assemble${type}
 uploadApk=`renameApk "./app/build/outputs/apk/app-${type}.apk" ${type}`
 
 
-checkJq
+
 
 if [ ${isUpload} == "true" ]
 then
